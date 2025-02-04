4f41a77093a0063d8fd8259d88e5451d28113c8c
ndri <5224431+ndri@users.noreply.github.com>
2025-02-04 15:56:16 +0100

    Improve task list design

--- src/components/TaskList.svelte
+++ src/components/TaskList.svelte
@@ -11,16 +11,15 @@
 
 {#if tasks?.length}
 	<section>
-		<h2 class="mb-4 text-xl font-medium">Tasks:</h2>
-		<ol class="ml-8 list-decimal">
+		<h2 class="mb-4 text-xl font-medium">Tasks</h2>
+		<ol class="flex flex-col divide-y-2 divide-solid divide-slate-900 rounded-xl bg-slate-800">
 			{#each tasks as task}
-				<li>
-					<div class="flex items-center gap-1.5">
-						<span>{task.name}</span>
-						<button class="rounded-md p-1 hover:bg-slate-700" onclick={() => deleteTask(task.id)}
-							><TrashIcon className="text-slate-200" /></button
-						>
-					</div>
+				<li class="flex items-center gap-2 px-5 py-3">
+					<div class="grow">{task.name}</div>
+					<button
+						class="flex justify-center rounded-md p-2.5 hover:bg-red-900"
+						onclick={() => deleteTask(task.id)}><TrashIcon className="text-slate-200" /></button
+					>
 				</li>
 			{/each}
 		</ol>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,8 @@
 	let { children } = $props();
 </script>
 
-<div class="text-100 flex min-h-full flex-col items-center bg-slate-900 p-16 text-slate-100">
-	<main class="flex w-full max-w-3xl flex-col gap-8 rounded-2xl bg-slate-800 p-16">
+<div class="text-100 flex min-h-full flex-col items-center bg-slate-950 p-16 text-slate-100">
+	<main class="flex w-full max-w-3xl flex-col gap-8 rounded-2xl bg-slate-900 p-16">
 		{@render children()}
 	</main>
 </div>
