c63014b1dcd75fb9d093879dabf70061567833fd
ndri <5224431+ndri@users.noreply.github.com>
2025-02-12 13:54:25 +0100

    Reverse project list order

--- src/lib/components/ProjectList.svelte
+++ src/lib/components/ProjectList.svelte
@@ -5,17 +5,16 @@
 	import NewProjectCard from './NewProjectCard.svelte';
 
 	const projectsQuery = stateQuery(getProjectsWithTasks);
-	let projects = $derived(projectsQuery.current);
+	let projects = $derived(projectsQuery.current ?? []);
+	let reversedProjects = $derived(projects.toReversed());
 </script>
 
-{#if projects?.length}
-	<section>
-		<h2 class="mb-4 text-xl font-medium">Your projects</h2>
-		<ul class="grid grid-cols-1 gap-3 md:grid-cols-2">
-			<NewProjectCard />
-			{#each projects as project}
-				<ProjectCard {project} />
-			{/each}
-		</ul>
-	</section>
-{/if}
+<section>
+	<h2 class="mb-4 text-xl font-medium">Your projects</h2>
+	<ul class="grid grid-cols-1 gap-3 md:grid-cols-2">
+		<NewProjectCard />
+		{#each reversedProjects as project}
+			<ProjectCard {project} />
+		{/each}
+	</ul>
+</section>
