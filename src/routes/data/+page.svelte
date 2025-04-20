cea1d7f3fbc61b320e2c2fd354d7291b0af84fc7
ndri <5224431+ndri@users.noreply.github.com>
2025-04-20 15:57:22 +0200

    Improve data page

--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -4,7 +4,6 @@
 	import Link from '$lib/components/Link.svelte';
 	import ProgressBar from '$lib/components/ProgressBar.svelte';
 	import Button from '$lib/components/ui/Button.svelte';
-	import Dialog from '$lib/components/ui/Dialog.svelte';
 	import {
 		formatBytes,
 		persist,
@@ -15,12 +14,13 @@
 
 <section class="flex flex-col gap-8">
 	<h2 class="text-2xl font-medium">Data</h2>
+	<div>Prioritizationator stores all data offline in your browser.</div>
+	<h3 class="text-xl font-medium">Storage</h3>
 	{#await showEstimatedQuota()}
 		<p>Loading...</p>
 	{:then estimatedQuota}
 		{#if estimatedQuota && estimatedQuota.quota && estimatedQuota.usage}
 			{@const usagePercentage = Math.round((estimatedQuota.usage / estimatedQuota.quota) * 100)}
-			<div>Prioritizationator stores all data offline in your browser.</div>
 			<div class="flex flex-col gap-2">
 				<div class="text-main-500 dark:text-main-400">
 					<span class="text-main-700 dark:text-main-300 font-medium">
@@ -34,49 +34,55 @@
 				</div>
 				<ProgressBar progress={estimatedQuota.usage} total={estimatedQuota.quota} size="lg" />
 			</div>
-			<div
-				class="border-main-200 dark:border-main-800 flex items-center gap-6 rounded-lg border p-6"
-			>
-				{#await tryPersistWithoutPromtingUser()}
-					<p>Loading...</p>
-				{:then result}
-					{#if result === 'persisted'}
-						<CheckCircleIcon class="size-12 shrink-0 text-green-600" />
-						<p>
-							Your data is persisted and it won't be deleted by the browser unless you delete it
-							manually.
-						</p>
-					{:else if result === 'prompt' || result === 'never'}
-						<ExclamationTriangleIcon class="text-main-400 dark:text-main-500 size-12 shrink-0" />
-						<div class="flex flex-col items-start gap-4">
+			<div class="flex flex-col gap-6">
+				<h3 class="text-xl font-medium">Persistence</h3>
+				<div
+					class={[
+						'flex items-center gap-6 rounded-lg border p-6',
+						'border-main-200 dark:border-main-800 bg-main-50 dark:bg-main-900 '
+					]}
+				>
+					{#await tryPersistWithoutPromtingUser()}
+						<p>Loading...</p>
+					{:then result}
+						{#if result === 'persisted'}
+							<CheckCircleIcon class="size-12 shrink-0 text-green-600" />
 							<p>
-								Your data <Link
-									href="https://developer.mozilla.org/en-US/docs/Web/API/Storage_API/Storage_quotas_and_eviction_criteria#does_browser-stored_data_persist"
-									newTab
-								>
-									might be deleted
-								</Link> when your device needs to make space.
+								Your data is persisted and it won't be deleted by the browser unless you delete it
+								manually.
 							</p>
-							{#if result === 'prompt'}
-								<Button
-									variant="primary"
-									onclick={async () => {
-										const success = await persist();
-										if (!success) {
-											alert(
-												'Your browser did not allow enabling persistent storage. Try to use the app some more and then try again.'
-											);
-										}
-									}}
-								>
-									Enable persistent storage
-								</Button>
-							{:else}
-								<p>Consider upgrading to a web browser that supports persistent storage.</p>
-							{/if}
-						</div>
-					{/if}
-				{/await}
+						{:else if result === 'prompt' || result === 'never'}
+							<ExclamationTriangleIcon class="text-main-400 dark:text-main-500 size-12 shrink-0" />
+							<div class="flex flex-col items-start gap-4">
+								<p>
+									Your data <Link
+										href="https://developer.mozilla.org/en-US/docs/Web/API/Storage_API/Storage_quotas_and_eviction_criteria#does_browser-stored_data_persist"
+										newTab
+									>
+										might be deleted
+									</Link> when your device needs to make space.
+								</p>
+								{#if result === 'prompt'}
+									<Button
+										variant="primary"
+										onclick={async () => {
+											const success = await persist();
+											if (!success) {
+												alert(
+													'Your browser did not allow enabling persistent storage. Try to use the app some more and then try again.'
+												);
+											}
+										}}
+									>
+										Enable persistent storage
+									</Button>
+								{:else}
+									<p>Consider upgrading to a web browser that supports persistent storage.</p>
+								{/if}
+							</div>
+						{/if}
+					{/await}
+				</div>
 			</div>
 		{:else}
 			<p>Unable to estimate storage.</p>
